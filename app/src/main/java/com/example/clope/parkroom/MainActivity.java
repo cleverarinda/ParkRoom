a0783dc87182ab371e85dbbdf71ae3ec3d4c1a2f
cleverarinda <cleverarinda@gmail.com>
2019-03-05 10:14:42 -0800

    Initial commit

---
+++ app/src/main/java/com/example/clope/parkroom/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.clope.parkroom;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_main);
+        Handler handler= new Handler ();
+        handler.postDelayed (new Runnable () {
+            @Override
+            public void run() {
+                Intent intent= new Intent (MainActivity.this, NearestParking.class);
+                startActivity (intent);
+                finish ();
+            }
+        },3000);
+    }
+}
